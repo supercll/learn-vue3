9b29bbbd988348c920d788962afe46e86f9f6d7e
supercll <1033581609@qq.com>
2022-07-21 18:46:29 +0800

    feat: toRef、toRefs、proxyRefs

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -18,6 +18,47 @@ export function shallowRef(value) {
   return createRef(value, true)
 }
 
+export function toRef(object, key) {
+  return new ObjectRefImpl(object, key)
+}
+
+export function toRefs(object) {
+  let result = {}
+  for (let key in object) {
+    result[key] = toRef(object, key)
+  }
+  return result
+}
+export function proxyRefs(objectWithRefs) {
+  // 代理的思想，如果是ref 则取ref.value
+  return new Proxy(objectWithRefs, {
+    get(target, key, receiver) {
+      let v = Reflect.get(target, key, receiver)
+      return v.__v_isRef ? v.value : v
+    },
+    set(target, key, value, receiver) {
+      // 设置的时候如果是ref,则给ref.value赋值
+      const oldValue = target[key]
+      if (oldValue.__v_isRef) {
+        oldValue.value = value
+        return true
+      } else {
+        return Reflect.set(target, key, value, receiver)
+      }
+    },
+  })
+}
+
+class ObjectRefImpl {
+  private __v_isRef = true
+  constructor(public object, public key) {}
+  get value() {
+    return this.object[this.key]
+  }
+  set value(newValue) {
+    this.object[this.key] = newValue
+  }
+}
 class RefImpl {
   private _value
   private dep
